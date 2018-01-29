8b74ef562e80e69878eeda756590ddb51244198b
sohail961 <17100283@lums.edu.pk>
2018-01-29 15:29:11 +0000

    root added

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,6 @@ Rails.application.routes.draw do
     end
 
   end
-  
-  # resources :posts do
-  #   member do
-  #     get 'comments'
-  #   end
-  #   collection do
-  #     post 'bulk_upload'
-  #   end
-  # end
-
+  root 'rand_apps#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
